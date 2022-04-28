d758411e66f743db480e3b60da63057ba16699f9
matiasrodriguez <matiasrodriguez@gmail.com>
2022-04-28 02:59:47 -0400

    clase 2

---
+++ clase 2/a_2.js
@@ -0,0 +1,18 @@
+alert("Ejercicio Asincronico 03");
+var num=prompt("Ingrese un numero");
+alert(num%2==0 ? "el numero es par" : "el numero es impar" );
+
+//----------------------------------
+alert("Ejercico Asincronico 04 Parte 1");
+var edad=prompt("Ingrese su edad si es mujer");
+if(edad>60){
+    alert("usted ya es una jubilada");
+}else{
+    alert("Usted es menor para jubilarse");
+};
+//-------------------------------
+alert("PARTE 2")
+var edad2=prompt("cuantos años tiene actualmente");
+var sexo= prompt("escriba si es hombre o mujer");
+if (sexo=="hombre" && edad2>= 65 || sexo=="mujer" && edad2>= 60 ) {alert("usted es " + sexo+ " con edad de "+edad2+ " tiene la edad de jubilacion");}
+else{alert("usted es " + sexo+ " con edad de "+edad2+ " no tiene la edad de jubilacion");}
---
+++ clase2_condisiones.js
@@ -0,0 +1,46 @@
+var edad = prompt("ingrese su edad")
+
+if (edad>=18) {alert ("UD, es mayor de edad");
+}else{
+    alert("Ud. es menor de edad");
+}
+menor = 18-edad
+if (menor )
+
+
+
+//multievaluacion
+
+var respuesta = prompt("cuanto es 1+1? :A) 3 -B)5 - C)0 - D)2");
+
+if (respuesta=="A"){alert("respuesta incorrecta")}
+;else if
+if (respuesta=="B"){alert("respuesta incorrecta");}
+else if (respuesta=="C"){alert("respuesta incorrecta");}
+else if (respuesta=="D"){alert("respuesta correcta!!!");}
+else{ alert("debe ingresar una alternativa entre A y D")}
+
+//desafio de operaciones matematica
+
+var num1 = prompt("ingrese primer numero")
+var num1 = prompt("ingrese segundo numero")
+var eleccion = prompt("escoga opcion : suma, resta, division, multiplicacion")
+//------------------------------------------------
+// pregunta 
+
+ancho = prompt("ingrese ancho en cm")
+largo = prompt("ingrese largo en cm")
+alert(ancho=ancho)
+
+
+
+
+
+var num1 =prompt("ingrese el primer numero");
+var num2 =prompt("ingrese el segundo numero");
+if(a===b)
+    alert("son iguales");
+if(a>b)
+    alert("el numero mayor es : "+a);
+if(b>a)
+    alert("el numero mayor es : "+b);
